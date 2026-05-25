3eff74b24ee618473ec1f10e7da92a595fc83af2
Amir <li25219@bristol.ac.uk>
2026-05-25 22:55:16 +0100

    day 1 + prelude

---
+++ src/AoCPrelude.hs
@@ -0,0 +1,142 @@
+-- All of the following code is taken from ratherforky's AoC (see README)
+-- Some comments have been added by me
+
+{-# language QuasiQuotes #-}
+{-# OPTIONS_GHC -Wno-missing-signatures #-}
+module AoCPrelude (
+  runDay,
+  module Data.List,
+  -- module Data.String.Interpolate,
+  multi, -- QuasiQuoter
+  module Flow,
+  -- Parsing
+  module Text.Yoda,
+  parseInput, digit, int, intSigned, integral,
+  everyLine, manyIgnoreWhitespace, tok, delim, delim',
+  whitespace, spaces, newlineP,
+  -- module Text.Pretty.Simple,
+  void,
+  -- Misc
+  applyN, applyAll, applyNScan,
+  scanUntilNothing,
+  within, betweenOrd, firstJusts,
+  converge
+  ) where
+
+import Data.List
+import Data.String.Interpolate ( i )
+import Data.String.QQ (s)
+import Flow
+-- import Text.Megaparsec hiding (State)
+-- import Text.Megaparsec.Char
+import Text.Yoda
+import Control.Monad (void)
+import Data.Maybe ( fromJust )
+-- import Text.Pretty.Simple
+import Data.Foldable ( msum )
+import Data.Char ( isDigit, isSpace )
+
+-- Renaming `i` QuasiQuoter to `multi` to avoid name conflicts
+multi = s
+
+runDay :: Int -> (String -> b) -> IO b
+runDay day task = task <$> readFile [i|src/inputs/Day#{day}Input.txt|]
+
+parseInput :: Parser a -> String -> a
+parseInput parser = parseMaybe parser .> fromJust -- === fromJust . parseMaybe parser
+
+-- run parser on every line
+everyLine :: Parser a -> Parser [a]
+everyLine parser = many (parser <* (newlineP <|> eof))
+
+newlineP :: Parser ()
+newlineP = void (char '\n')
+
+manyIgnoreWhitespace :: Parser a -> Parser [a]
+manyIgnoreWhitespace parser = many (parser <* (whitespace <|> eof))
+
+whitespace :: Parser ()
+whitespace = () <$ many (satisfy isSpace)
+
+spaces :: Parser ()
+spaces = () <$ many (char ' ')
+
+int :: Parser Int
+int = read <$> some digit
+
+digit :: Parser Char
+digit = satisfy isDigit
+
+integral :: (Integral a, Read a) => Parser a
+integral = read <$> some digit
+
+
+intSigned :: Parser Int
+intSigned
+  = choice
+      [ char '-' *> (negate <$> int)
+      , int
+      ]
+
+tok :: String -> Parser ()
+tok str = whitespace *> string str *> whitespace
+
+delim :: Parser a -> String -> Parser c -> Parser [a]
+delim elementP delimiter terminatorP
+  = delim' (pure ()) elementP (tok delimiter) terminatorP
+
+-- >>> parse (delim digit "," eof) "4,5"
+-- [("45","")]
+
+delim' :: Parser start -> Parser a -> Parser delim -> Parser end -> Parser [a]
+delim' startP elementP delimiterP terminatorP = do
+  _ <- startP
+  elements <- many (elementP <* delimiterP)
+  lastElement <- elementP
+  _ <- terminatorP
+  pure (elements ++ [lastElement])
+
+
+applyN :: Int -> (a -> a) -> a -> a
+applyN n0 f = go n0
+  where
+    go 0 a = a
+    go n a = go (n-1) (f a)
+
+applyAll :: [a -> a] -> a -> a
+applyAll fs x = foldl' (flip ($)) x fs
+
+applyNScan :: Int -> (a -> a) -> a -> [a]
+applyNScan n0 f = go n0
+  where
+    go 0 a = [a]
+    go n a = a : go (n-1) (f a)
+
+scanUntilNothing :: (a -> Maybe a) -> a -> [a]
+scanUntilNothing f = go
+  where
+    go x = case f x of
+      Nothing -> [x]
+      Just x' -> x : go x'
+
+
+within :: Int -> Int -> Int -> Bool
+within n x y = abs (x - y) <= n
+
+betweenOrd :: Ord a => a -> a -> a -> Bool
+betweenOrd maxX minX x = maxX >= x && x >= minX
+
+-- From GHC.Data.Maybe
+-- | Takes a list of @Maybes@ and returns the first @Just@ if there is one, or
+-- @Nothing@ otherwise.
+firstJusts :: Foldable f => f (Maybe a) -> Maybe a
+firstJusts = msum
+{-# SPECIALISE firstJusts :: [Maybe a] -> Maybe a #-}
+
+converge :: Eq a => (a -> a) -> a -> a
+converge f k = go $ iterate f k
+  where
+    go (x:y:ys)
+      | x == y    = x
+      | otherwise = go (y:ys)
+    go _ = error "iterate is infinite, so this should never happen"
---
+++ src/Day1.hs
@@ -0,0 +1,56 @@
+module Day1 where
+
+import AoCPrelude
+
+data Direction = L | R deriving (Show, Enum, Eq)
+type Turn = Int
+
+dirParse :: Parser Direction
+dirParse = toDir <$> (char 'L' <|> char 'R')
+    where
+        toDir :: Char -> Direction
+        toDir 'L' = L
+        toDir 'R' = R
+        toDir _ = error "should be unreachable"
+
+instrParse :: Parser Turn
+instrParse = do
+    dir <- dirParse
+    val <- int
+    if dir == L then pure $ -val else pure val
+
+initParse :: String -> [Turn]
+initParse = fmap (parseInput instrParse) . words
+
+egInput :: String
+egInput = "L68 L30 R48 L5 R60 L55 L1 L99 R14 L82"
+
+rotate :: Int -> Turn -> Int
+rotate acc turn
+    | turn < 0 = (100 + acc + turn) `mod` 100
+    | otherwise =      (acc + turn) `mod` 100
+
+dials :: [Turn] -> [Int]
+dials = scanl rotate 50
+
+task1 :: String -> Int
+task1 = length . filter (== 0) . dials . initParse
+
+zeroes :: [Turn] -> [Int]
+zeroes = liftA2 (zipWith crosses) dials id
+
+crosses :: Int -> Turn -> Int
+crosses dial turn
+    | dial == 0 && turn >= -100 && turn < 0 = 0 -- this edgecase cost me an hour
+    | dial + turn < 0 = 1 + crosses dial (100 + turn)
+    | dial + turn > 100 = 1 + crosses dial (turn - 100)
+    | otherwise = 0
+
+task2 :: String -> Int
+task2 = liftA2 (+) (sum . zeroes . initParse) task1
+
+runTask1 :: IO Int
+runTask1 = runDay 1 task1
+
+runTask2 :: IO Int
+runTask2 = runDay 1 task2
